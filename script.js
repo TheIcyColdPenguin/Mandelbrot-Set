e6997e8812e7c13f80a1744a23b296e72d503aaf
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2020-12-16 00:58:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,157 @@
+var sketch = new p5(function (p) {
+    // useful interfaces
+    // variables that decide how to display the set
+    // let cartCordesians: Range = { x: { start: -2, stop: 2 }, y: { start: -2, stop: 2 } };
+    var cartCordesians = { x: { start: -2.5, stop: 1.5 }, y: { start: -2, stop: 2 } };
+    var maxIterations = 50;
+    var escapeRadius = 500;
+    var zoomValue = 0;
+    // sliders to adjust above variables
+    var maxIterationsSlider;
+    p.setup = function () {
+        var windowSize = Math.min(p.windowHeight, p.windowWidth) - 20;
+        p.createCanvas(windowSize, windowSize);
+        p.pixelDensity(1);
+        p.frameRate(20);
+        p.textSize(32);
+        showText();
+        maxIterationsSlider = p.createSlider(3, 100, 50, 1);
+        maxIterationsSlider.position(20, 140);
+        maxIterationsSlider.input(function () {
+            maxIterations = maxIterationsSlider.value();
+        });
+    };
+    p.draw = function () {
+        p.background(51);
+        p.loadPixels();
+        for (var x = 0; x < p.width; x++) {
+            for (var y = 0; y < p.height; y++) {
+                // find what color each pixel should be
+                var col = mapRange(goesToInf(screenToCart({ x: x, y: y })), -10, 75, 0, 255);
+                var loc = (x + y * p.width) * 4;
+                p.pixels[loc + 0] = col * 0.9;
+                p.pixels[loc + 1] = col * 1.477;
+                p.pixels[loc + 2] = col * 1.428;
+                p.pixels[loc + 3] = 255;
+            }
+        }
+        p.updatePixels();
+    };
+    p.windowResized = function () {
+        var windowSize = Math.min(p.windowHeight, p.windowWidth) - 20;
+        p.resizeCanvas(windowSize, windowSize);
+    };
+    p.mouseDragged = function () {
+        if (p.mouseX >= 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY < p.height) {
+            var offset = convertSliderToLog(zoomValue);
+            var movement = {
+                real: offset * -2e-3 * (p.mouseX - p.pmouseX),
+                imag: offset * -2e-3 * (p.mouseY - p.pmouseY)
+            };
+            cartCordesians.x.start += movement.real;
+            cartCordesians.x.stop += movement.real;
+            cartCordesians.y.start += movement.imag;
+            cartCordesians.y.stop += movement.imag;
+        }
+    };
+    // helper functions
+    var screenToCart = function (_a) {
+        // desired range = [-2,    2   ]  (for each direction)
+        // input   range = [ 0, width-1]  (for each direction)
+        var x = _a.x, y = _a.y;
+        return {
+            real: mapRange(x, 0, p.width, cartCordesians.x.start, cartCordesians.x.stop),
+            imag: mapRange(y, 0, p.height, cartCordesians.y.start, cartCordesians.y.stop)
+        };
+    };
+    var goesToInf = function (c) {
+        // this is the main function that we iterate multiple times
+        var iterate = function (z) {
+            var square = {
+                real: z.real * z.real - z.imag * z.imag,
+                imag: 2 * z.real * z.imag
+            };
+            return { real: square.real + c.real, imag: square.imag + c.imag };
+        };
+        // iterate multiple times until either MAX_ITERATIONS is reached
+        // or the value blows up
+        var zNext = iterate({ real: 0, imag: 0 });
+        var i = 0;
+        while (i < maxIterations) {
+            zNext = iterate(zNext);
+            if (magnitude(zNext) > escapeRadius)
+                break;
+            i++;
+        }
+        return i;
+    };
+    // function to calculate the magnitude of any complex number
+    var magnitude = function (z) {
+        return Math.sqrt(z.real * z.real + z.imag + z.imag);
+    };
+    // maps a number from one range to another
+    var mapRange = function (n, initialRangeStart, initialRangeStop, newRangeStart, newRangeEnd) {
+        var initialRange = initialRangeStop - initialRangeStart;
+        var newRange = newRangeEnd - newRangeStart;
+        return ((n - initialRangeStart) / initialRange) * newRange + newRangeStart;
+    };
+    var convertSliderToLog = function (val) {
+        // to convert a linearly increasing value to a logarithmically increasing one
+        var inputStart = 100;
+        var inputStop = 0;
+        var outputStart = Math.log(1e-10);
+        var outputStop = Math.log(2.3);
+        var scale = (outputStop - outputStart) / (inputStop - inputStart);
+        return Math.exp((val - inputStart) * scale + outputStart);
+    };
+    var showText = function () {
+        // helper functions
+        // function to setup and show all interactive elements
+        var insertText = function (text, x, y, size, onClickCallback) {
+            var label = document.createElement("p");
+            label.innerText = text;
+            label.className = "help-text";
+            label.style.left = x.toString() + "px";
+            label.style.top = y.toString() + "px";
+            label.style.fontSize = (size || 16).toString() + "px";
+            if (onClickCallback) {
+                label.className += " interactive";
+                label.style.cursor = "pointer";
+                label.addEventListener("click", onClickCallback);
+            }
+            document.body.insertAdjacentElement("beforeend", label);
+        };
+        var zoomIn = function () {
+            zoomValue += 1;
+            updateZoom();
+        };
+        var zoomOut = function () {
+            zoomValue -= 1;
+            updateZoom();
+        };
+        // calculates and changes the coordinates to
+        var updateZoom = function () {
+            // calculate canvas size
+            var offset = convertSliderToLog(zoomValue);
+            var prevCenter = {
+                x: (cartCordesians.x.start + cartCordesians.x.stop) / 2,
+                y: (cartCordesians.y.start + cartCordesians.y.stop) / 2
+            };
+            cartCordesians = {
+                x: { start: prevCenter.x - offset, stop: prevCenter.x + offset },
+                y: { start: prevCenter.y - offset, stop: prevCenter.y + offset }
+            };
+        };
+        insertText("Zoom", 35, 5);
+        insertText("-", 30, 15, 35, zoomOut);
+        insertText("+", 60, 25, 30, zoomIn);
+        insertText("Max iterations", 30, 95);
+        // keyboard shortcuts for zooming
+        document.addEventListener("keypress", function (e) {
+            if (e.code === "Equal")
+                zoomIn();
+            if (e.code === "Minus")
+                zoomOut();
+        });
+    };
+});
---
+++ script.ts
@@ -0,0 +1,227 @@
+declare const p5: any;
+
+const sketch = new p5((p: any) => {
+    // useful interfaces
+
+    interface Complex {
+        real: number;
+        imag: number;
+    }
+
+    interface Range {
+        x: { start: number; stop: number };
+        y: { start: number; stop: number };
+    }
+
+    // variables that decide how to display the set
+
+    // let cartCordesians: Range = { x: { start: -2, stop: 2 }, y: { start: -2, stop: 2 } };
+    let cartCordesians: Range = { x: { start: -2.5, stop: 1.5 }, y: { start: -2, stop: 2 } };
+
+    let maxIterations = 50;
+    let escapeRadius = 500;
+    let zoomValue = 0;
+
+    // sliders to adjust above variables
+
+    let maxIterationsSlider: any;
+
+    p.setup = () => {
+        const windowSize = Math.min(p.windowHeight, p.windowWidth) - 20;
+
+        p.createCanvas(windowSize, windowSize);
+        p.pixelDensity(1);
+        p.frameRate(20);
+
+        p.textSize(32);
+        showText();
+
+        maxIterationsSlider = p.createSlider(3, 100, 50, 1);
+        maxIterationsSlider.position(20, 140);
+        maxIterationsSlider.input(() => {
+            maxIterations = maxIterationsSlider.value();
+        });
+    };
+
+    p.draw = () => {
+        p.background(51);
+
+        p.loadPixels();
+
+        for (let x = 0; x < p.width; x++) {
+            for (let y = 0; y < p.height; y++) {
+                // find what color each pixel should be
+                let col: number = mapRange(goesToInf(screenToCart({ x, y })), -10, 75, 0, 255);
+
+                const loc = (x + y * p.width) * 4;
+                p.pixels[loc + 0] = col * 0.9;
+                p.pixels[loc + 1] = col * 1.477;
+                p.pixels[loc + 2] = col * 1.428;
+                p.pixels[loc + 3] = 255;
+            }
+        }
+
+        p.updatePixels();
+    };
+
+    p.windowResized = () => {
+        const windowSize = Math.min(p.windowHeight, p.windowWidth) - 20;
+        p.resizeCanvas(windowSize, windowSize);
+    };
+
+    p.mouseDragged = () => {
+        if (p.mouseX >= 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY < p.height) {
+            const offset = convertSliderToLog(zoomValue);
+
+            const movement = {
+                real: offset * -2e-3 * (p.mouseX - p.pmouseX),
+                imag: offset * -2e-3 * (p.mouseY - p.pmouseY),
+            };
+
+            cartCordesians.x.start += movement.real;
+            cartCordesians.x.stop += movement.real;
+
+            cartCordesians.y.start += movement.imag;
+            cartCordesians.y.stop += movement.imag;
+        }
+    };
+
+    // helper functions
+
+    const screenToCart = ({ x, y }: { x: number; y: number }): Complex => {
+        // desired range = [-2,    2   ]  (for each direction)
+        // input   range = [ 0, width-1]  (for each direction)
+
+        return {
+            real: mapRange(x, 0, p.width, cartCordesians.x.start, cartCordesians.x.stop),
+            imag: mapRange(y, 0, p.height, cartCordesians.y.start, cartCordesians.y.stop),
+        };
+    };
+
+    const goesToInf = (c: Complex): number => {
+        // this is the main function that we iterate multiple times
+
+        const iterate = (z: Complex) => {
+            const square: Complex = {
+                real: z.real * z.real - z.imag * z.imag,
+                imag: 2 * z.real * z.imag,
+            };
+            return { real: square.real + c.real, imag: square.imag + c.imag };
+        };
+
+        // iterate multiple times until either MAX_ITERATIONS is reached
+        // or the value blows up
+
+        let zNext = iterate({ real: 0, imag: 0 });
+
+        let i = 0;
+        while (i < maxIterations) {
+            zNext = iterate(zNext);
+            if (magnitude(zNext) > escapeRadius) break;
+
+            i++;
+        }
+
+        return i;
+    };
+
+    // function to calculate the magnitude of any complex number
+    const magnitude = (z: Complex): number => {
+        return Math.sqrt(z.real * z.real + z.imag + z.imag);
+    };
+
+    // maps a number from one range to another
+    const mapRange = (
+        n: number,
+        initialRangeStart: number,
+        initialRangeStop: number,
+        newRangeStart: number,
+        newRangeEnd: number
+    ): number => {
+        const initialRange = initialRangeStop - initialRangeStart;
+        const newRange = newRangeEnd - newRangeStart;
+
+        return ((n - initialRangeStart) / initialRange) * newRange + newRangeStart;
+    };
+
+    const convertSliderToLog = (val: number) => {
+        // to convert a linearly increasing value to a logarithmically increasing one
+
+        const inputStart = 100;
+        const inputStop = 0;
+
+        const outputStart = Math.log(1e-10);
+        const outputStop = Math.log(2.3);
+
+        const scale = (outputStop - outputStart) / (inputStop - inputStart);
+
+        return Math.exp((val - inputStart) * scale + outputStart);
+    };
+
+    const showText = () => {
+        // helper functions
+
+        // function to setup and show all interactive elements
+        const insertText = (
+            text: string,
+            x: number,
+            y: number,
+            size?: number,
+            onClickCallback?: () => void
+        ) => {
+            const label = document.createElement("p");
+
+            label.innerText = text;
+            label.className = "help-text";
+            label.style.left = x.toString() + "px";
+            label.style.top = y.toString() + "px";
+            label.style.fontSize = (size || 16).toString() + "px";
+
+            if (onClickCallback) {
+                label.className += " interactive";
+                label.style.cursor = "pointer";
+                label.addEventListener("click", onClickCallback);
+            }
+
+            document.body.insertAdjacentElement("beforeend", label);
+        };
+
+        const zoomIn = () => {
+            zoomValue += 1;
+            updateZoom();
+        };
+
+        const zoomOut = () => {
+            zoomValue -= 1;
+            updateZoom();
+        };
+
+        // calculates and changes the coordinates to
+        const updateZoom = () => {
+            // calculate canvas size
+            const offset = convertSliderToLog(zoomValue);
+
+            const prevCenter = {
+                x: (cartCordesians.x.start + cartCordesians.x.stop) / 2,
+                y: (cartCordesians.y.start + cartCordesians.y.stop) / 2,
+            };
+
+            cartCordesians = {
+                x: { start: prevCenter.x - offset, stop: prevCenter.x + offset },
+                y: { start: prevCenter.y - offset, stop: prevCenter.y + offset },
+            };
+        };
+
+        insertText("Zoom", 35, 5);
+        insertText("-", 30, 15, 35, zoomOut);
+        insertText("+", 60, 25, 30, zoomIn);
+
+        insertText("Max iterations", 30, 95);
+
+        // keyboard shortcuts for zooming
+        document.addEventListener("keypress", (e) => {
+            if (e.code === "Equal") zoomIn();
+            if (e.code === "Minus") zoomOut();
+        });
+    };
+});
